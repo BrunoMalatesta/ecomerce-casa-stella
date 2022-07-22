9cf844f92608fbde75f68d910d27e81e953758f3
Bruno Malatesta <6malacabeza9@gmail.com>
2022-07-22 17:34:46 -0300

    cambiecosas22/7/22

--- casaestella2.js
+++ casaestella2.js
@@ -1,268 +1,15 @@
-/* ARRAY PRODUCTOS*/
-const lista_productos =[
-    {
-        id: 0,
-        img: "./productos/tijeras.jpg",
-        nombre: "Tijera",
-        precio: 550,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 1,
-        img: "./productos/plasticola.jpg",
-        nombre: "Plasticola",
-        precio: 200,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 2,
-        img: "./productos/lapicera.jpg",
-        nombre: "Lapicera",
-        precio: 190,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 3,
-        img: "./productos/pinceles.jpg",          
-        nombre: "Pinceles",
-        precio: 400,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 4,
-        img:  "./productos/acrilicos.jpg",        
-        nombre: "Acrilicos",
-        precio: 2800,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 5,
-        img: "./productos/resma.jpg",     
-        nombre: "Resma",
-        precio: 890,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 6,
-        img: "./productos/lapiz.jpg",
-        nombre: "Lapiz",                             
-        precio: 160,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 7,
-        img: "./productos/regla.jpg",
-        nombre: "Regla",
-        precio: 330,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 8,
-        img: "./productos/mapa.jpg",
-        nombre: "Mapa",
-        precio: 50,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 9,
-        img: "./productos/goma.jpg",
-        nombre: "Goma Maped",
-        precio: 180,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 10,
-        img: "./productos/mochila.jpg",
-        nombre: "Mochila Verde Musgo",
-        precio: 7699,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 11,
-        img: "./productos/mochila2.jpg",
-        nombre: "Mochila Wilson",
-        precio: 6100,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 12,
-        img: "./productos/mochila3.jpg",
-        nombre: "Mochila Wilson Bicolor",
-        precio: 6900,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 13,
-        img: "./productos/billetera.jpg",
-        nombre: "Billetera Hombre Duogan",
-        precio: 2980,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 14,
-        img: "./productos/billetera2.jpg",
-        nombre: "Billetera Hombre Duogan Eco Cuero",
-        precio: 2980,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 15,
-        img: "./productos/billetera3.jpg",
-        nombre: "Billetera  PlayStation One",
-        precio: 3000,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 16,
-        img: "./productos/cajalapicez.jpg",
-        nombre: "Lápíces De Colores Filgo",
-        precio: 2500,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 17,
-        img: "./productos/sharpie.jpg",
-        nombre: "Marcador Sharpie",
-        precio: 250,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 18,
-        img: "./productos/kit.jpg",
-        nombre: "Kit Set Utiles Escolares Gloria",
-        precio: 1000,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 19,
-        img: "./productos/lapicescolores.jpg",
-        nombre: "Lapices Filgo Color",
-        precio: 300,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 20,
-        img: "./productos/marcadorescolores.jpg",
-        nombre: "Colores De Resaltador Filgo",
-        precio: 1650,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 21,
-        img: "./productos/tintamarcador.jpg",
-        nombre:"Tinta Trabi 30 Ml",
-        precio: 900,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 22,
-        img: "./productos/kit2.jpg",
-        nombre: "Kit Utiles Escolares K26",
-        precio: 2420,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 23,
-        img: "./productos/marcadorpizzarra.jpg",
-        nombre: "Marcador Pizarra Negro Imantado",
-        precio: 2100,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 24,
-        img: "./productos/plumaparker.jpg",
-        nombre: "Boligrafo Parker Jotter Acero",
-        precio: 4000,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 25,
-        img: "./productos/kitcolorcastell.jpg",
-        nombre: "Lapices De Color FaberCastell",
-        precio: 6000,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 26,
-        img: "./productos/sello.jpg",
-        nombre: "Sellos Personalizados",
-        precio: 800,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 27,
-        img: "./productos/cartulina.jpg",
-        nombre: "Papel Afiche 70 X 100 X 10 Unidades ",
-        precio: 600,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 28,
-        img: "./productos/separadores.jpg",
-        nombre: "Separador Cartulina A4",
-        precio: 200,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 29,
-        img: "./productos/pizarra1.jpg",
-        nombre: "Pizarra Blanca 60x80cm",
-        precio: 3000,
-        cantidad: 1,
-        stock: 100,
-    },
-    {
-        id: 30,
-        img: "./productos/pizarra2.jpg",
-        nombre: "Pizarra Blanca 80x120cm",
-        precio: 6000,
-        cantidad: 1,
-        stock: 100,
-    },
-];
-
-
-/* ARRAY VACIO CARRITO*/
-let carrito = [];
-
-
-/* VARIABLES*/
+/* VARIABLES DOM*/
 let contenedorCarrito = document.getElementById('carrito-contenedor')
 let contenedorProductos = document.getElementById('contenedor-Productos')
 let contadorCarrito = document.getElementById('contadorCarrito')
 let botonVaciar = document.getElementById('vaciar-carrito')
 let botonComprar = document.getElementById('comprar-carrito')
-let botonTachito = document.getElementsByClassName('boton-eliminar')       
-/* INJECTAR PRODUCTOS*/
+let botonTachito = document.getElementsByClassName('boton-eliminar')
+
+/* ARRAY VACIO CARRITO*/
+let carrito = [];
+
+/* IMPRIMIR ARRAY PRODUCTOS EN CARDS Y ALERTA DEL BOTON PARA AGREGAR AL CARRITO*/
 lista_productos.forEach((info) => {
 
     let div = document.createElement('div');
@@ -285,19 +32,19 @@ lista_productos.forEach((info) => {
             destination: "https://github.com/apvarun/toastify-js",
             newWindow: true,
             close: true,
-            gravity: "top", // `top` or `bottom`
-            position: "right", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
+            gravity: "top",
+            position: "right",
+            stopOnFocus: true, 
             style: {
               background: "blue",
             },
-            onClick: function(){} // Callback after click
+            onClick: function(){} 
           }).showToast();
     })
 });
 
 
-/* AGG AL CARRITO SIN QUE SE REPITA*/
+/* AGG PRODUCTO AL CARRITO SIN QUE SE REPITA*/
 const agregarAlCarrito = (prodId) => {
     let agregar_producto = lista_productos.find(prod => prod.id == prodId)
     let existe = carrito.some(prod => prod.id === prodId)
@@ -310,7 +57,7 @@ const agregarAlCarrito = (prodId) => {
 
 
 
-/* ACTUALIZAR CARRITO*/
+/* IMPRIMIR EL CARRITO*/
 const actualizarCarrito = () => {
     contenedorCarrito.innerHTML = ""
     carrito.forEach((info) => {
@@ -329,14 +76,12 @@ const actualizarCarrito = () => {
 
     })
     
-    
-
     console.log(carrito)
     precioTotal.innerText = carrito.reduce((acc, info) => acc + info.cantidad * info.precio, 0)
     
 };
 
-
+/*CARGA EL HTML Y OBTIENE EL LOCALSTORAGE DE CARRITO*/
 document.addEventListener('DOMContentLoaded', () => {
     if (localStorage.getItem('carrito')){
         carrito = JSON.parse(localStorage.getItem('carrito'))
@@ -344,7 +89,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 })
 
-/* EliminardelCARRITO*/
+/* BOTON ELIMAR CON EL TACHITO DEL CARRITO Y ALERTAS*/
 const eliminarDelCarrito = (infoId) => {
     const existe = carrito.some(prod => prod.id ===infoId)
     if(existe){
@@ -362,13 +107,13 @@ const eliminarDelCarrito = (infoId) => {
                 duration: 2000,
                 newWindow: true,
                 close: true,
-                gravity: "top", // `top` or `bottom`
-                position: "right", // `left`, `center` or `right`
-                stopOnFocus: true, // Prevents dismissing of toast on hover
+                gravity: "top", 
+                position: "right", 
+                stopOnFocus: true, 
                 style: {
                   background: "red",
                 },
-                onClick: function(){} // Callback after click
+                onClick: function(){} 
               }).showToast();
         })
      
@@ -382,24 +127,7 @@ const eliminarDelCarrito = (infoId) => {
 };
 
 
-
-
-
-/*VACIAR CARRITO*/
-
-botonVaciar.addEventListener("click", () => {
-    localStorage.removeItem('carrito', JSON.stringify(carrito)),
-
-    contenedorCarrito.innerHTML = "",
-    
-    precioTotal.innerText = 0,
-  
-    carrito = []
-  
-});
-
-
-/*Boton Comprar*/
+/*BOTON SIMULAR COMPRA Y SUS ALERTAS*/
 botonComprar.addEventListener('click', () => {
     
 const swalWithBootstrapButtons = Swal.mixin({
@@ -464,7 +192,7 @@ modalCarrito.addEventListener('click', (event) => {
 
 
 
-
+/* ANIMACIONES ANIME.JS PARA CARDS Y NAV*/
   anime({
     targets: '.producto',
     scale: [
